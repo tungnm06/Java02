d56afd5d6afe8beca2b9acb2bb749ff4b21165d5
tungnm06 <tungnmd00439@fpt.edu.vn>
2017-08-26 10:09:24 +0700

    Upload project

---
+++ src/ss1/Ss1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Ss1 {
+
+    public static void main(String[] args) {
+        String names = " Nam, Truong, Dung, Danh, Tung, Khanh, Linh, Dong, Chien, Truong2, Ngoc, Toản, Toàn";
+        String[] arrayName = names.split(",");
+        for (int i = 0; i < arrayName.length; i++) {
+            System.out.println(i+1+". "+arrayName[i].trim());
+        }
+        System.out.println("1." + arrayName[0]);
+        System.out.println("2." + arrayName[1]);
+
+    }
+}
