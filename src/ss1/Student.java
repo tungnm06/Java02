e58ebb5fde8c0cc0cf43893290a11c3c3b825522
tungnm06 <tungnmd00439@fpt.edu.vn>
2017-08-26 10:47:14 +0700

    Them moi class student.

---
+++ src/ss1/Student.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss1;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Student {
+    private String name;
+    private String email;
+    private String birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+    
+}
