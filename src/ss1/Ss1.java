f4470ed1b5f0d0c7fdced1d61800f3338ca6eb1a
tungnm06 <tungnmd00439@fpt.edu.vn>
2017-08-26 11:18:47 +0700

    buoi hoc so 1

--- src/ss1/Ss1.java
+++ src/ss1/Ss1.java
@@ -5,21 +5,38 @@
  */
 package ss1;
 
-/**
- *
+import java.util.ArrayList;
+
+/*
+ *String names = " Nam, Truong, Dung, Danh, Tung, Khanh, Linh, Dong, Chien, Truong2, Ngoc, Toản, Toàn";
+        String[] arrayName = names.split(",");
+        for (int i = 0; i < arrayName.length; i++) {
+            System.out.println(i + 1 + ". " + arrayName[i].trim());
+            System.out.println("KaKa");
  * @author Administrator
  */
 public class Ss1 {
 
     public static void main(String[] args) {
-        String names = " Nam, Truong, Dung, Danh, Tung, Khanh, Linh, Dong, Chien, Truong2, Ngoc, Toản, Toàn";
-        String[] arrayName = names.split(",");
-        for (int i = 0; i < arrayName.length; i++) {
-            System.out.println(i + 1 + ". " + arrayName[i].trim());
-            System.out.println("KaKa");
-        }
-        
-        
+        Student student01 = new Student();
+        student01.setName("Truong");
+        student01.setEmail("truong@gmail.com");
+        student01.setBirthday("22/12/1995");
+        Student student02 = new Student();
+        student02.setName("Hung");
+        student02.setEmail("Hung@gmail.com");
+        student02.setBirthday("22/12/1996");
 
+        ArrayList<Student> listStudent = new ArrayList<>();
+        listStudent.add(student01);
+        listStudent.add(student02);
+        //        for(Student student:listStudent){
+        //           System.out.println(student.getName());
+        //     }
+         listStudent.remove(0);
+        Student st = listStudent.get(0);
+        System.out.println(st.getName());
+        
     }
+
 }
